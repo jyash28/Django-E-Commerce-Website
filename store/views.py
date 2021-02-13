0cde54fb396dfaee201864ba95030f92f0f8699e
jyash28 <jyash548@gmail.com>
2021-02-13 14:00:04 +0530

    second

--- store/views.py
+++ store/views.py
@@ -29,12 +29,36 @@ def signup(request):
         phone=postData.get('phone')
         email=postData.get('email')
         password=postData.get('password')
-        print(first_name,last_name,phone,email,password)
-        customer= Customer(first_name=first_name,
-                           last_name=last_name,
-                           phone=phone,
-                           email=email,
-                           password=password)
-        customer.register()
-        return HttpResponse( "sign up success")
+
+        # validation
+        error_message=None
+
+        if(not first_name):
+            error_message="First Name Required"
+        elif len(first_name)<4 :
+            error_message= "First name should be minimum 4 character "
+        elif  not last_name:
+            error_message="Lastname Required!!"
+        elif len(last_name)<4:
+            error_message="Last name should be minimum 4 character "
+        elif not phone:
+            error_message='Phone Number Required!!'
+        elif len(phone)<10:
+            error_message="Phone number must be minimum 10 digit"
+        elif len(password)<8:
+            error_message="password must be minimum 8 character"
+        elif len(email)<6:
+            error_message="email must be minimum 6 character"
+
+        # saving
+        if not error_message:
+            print(first_name,last_name,phone,email,password)
+            customer= Customer(first_name=first_name,
+                            last_name=last_name,
+                            phone=phone,
+                            email=email,
+                            password=password)
+            customer.register()
+        else:
+            return render(request,'signup.html',{'error':error_message})
 
