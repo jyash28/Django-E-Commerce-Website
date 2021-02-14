7c561f24874c4e6370d09a0a0b05016fda8878a2
jyash28 <jyash548@gmail.com>
2021-02-14 15:28:58 +0530

    update

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,8 @@
-from .views import index,signup
+from .views import index,signup,login
 from django.urls import path
 
 urlpatterns = [
     path('',index,name='homepage'),
-    path('signup',signup)
+    path('signup',signup),
+    path('login',login)
 ]   
